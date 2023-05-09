3891a3e958a11896769c163ec15fc803dbed40f0
saif58475 <saiffagal@hotmail.com>
2023-05-09 13:15:54 +0200

    new builded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'website', loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
