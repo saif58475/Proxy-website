38d1a9198d7c0626f28e136a9ffccc9f3be1bdf5
saif58475 <saiffagal@hotmail.com>
2023-05-15 15:37:04 +0200

    modification of 15/5

--- src/app/website/website.component.ts
+++ src/app/website/website.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website.component.css']
 })
 export class WebsiteComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
-  }
+    
+  }    
+    
+
 
 }
