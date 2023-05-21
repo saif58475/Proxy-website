e10e5dee152c9979d240fa8b5f622362f4a66f58
saif58475 <saiffagal@hotmail.com>
2023-05-21 13:37:24 +0200

    modification in components

--- src/app/website/website.component.ts
+++ src/app/website/website.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2  } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-website',
@@ -6,27 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website.component.css']
 })
 export class WebsiteComponent implements OnInit {
-    map:string = 'assets/images/mapKSA.png';  
-  constructor() { }
+    map:string = 'assets/images/mapKSA.png'; 
+    countries:string [] = ['ksa','uae','egy']; 
+    element:any;
+    intervalId: any;
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    
+    let i = 0 ;
+    interval(2000).subscribe(() => {
+        this.SelectCountry(this.countries[i]);
+        // this.changecolor(`#${this.countries[i]}`);
+        if( i == 2){
+          i = 0; 
+       }else {
+        i++;
+       }
+    });
+   
   }    
-    
+
+
 SelectCountry(map:string){
   switch (map) {
     case "egy":
-      this.map = 'assets/images/mapEGY.png'
+      this.map = 'assets/images/mapEGY.png';
+       
       break;
     case "ksa":
-      this.map = 'assets/images/mapKSA.png'
+      this.map = 'assets/images/mapKSA.png';
+            
         break;
     case "uae":
-      this.map = 'assets/images/mapUAE.png'
+      this.map = 'assets/images/mapUAE.png';
+      
         break;
     default:
       this.map = 'assets/images/mapKSA.png'
   }
 }
 
+changecolor(id:string){
+  this.element = [];
+  this.element = this.elRef.nativeElement.querySelector(`#${id}`);
+  }
 }
