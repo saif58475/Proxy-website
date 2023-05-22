8af820a51ef9e8ba483b5f73f7efee1d5c68e129
saif58475 <saiffagal@hotmail.com>
2023-05-22 12:27:28 +0200

    added the switch of the language

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,52 @@
 import { Component, ViewChild,OnInit, ElementRef } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  lang:boolean = false;
+  public languages: any[] = [{
+    language: 'English',
+    code: 'en',
+    type: 'US',
+    icon: 'us'
+  },{
+    language: 'Arabic',
+    code: 'ar',
+    type: 'EG',
+    icon: 'sa'
+  },
+ ]
   constructor() { }
 
   ngOnInit(): void {
    this.selectpage('one');
   }
 
+  changeLanguage() {
+    this.lang = !this.lang;
+    switch(this.lang) {
+      case true:
+      localStorage.setItem('language', 'en');
+      alert(      localStorage.getItem('language')      )
+      break;
+      case false:
+       localStorage.setItem('language', 'ar');
+       alert(      localStorage.getItem('language')      )
+      break;
+      default:
+        alert('nothing');
+    }
+
+    // if( localStorage.getItem('language') == 'en'){
+    //  alert('this is english');
+    // }else{
+    //   alert('this is arabic')
+    // }
+    
+  }
+  
   selectpage(page:string){
     switch (page) {
       case 'one':
